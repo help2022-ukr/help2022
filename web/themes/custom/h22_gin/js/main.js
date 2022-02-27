29b95c234c3e34970608d8ba9bfa120df43d1ec3
Sergiu Nagailic <nikro.md@gmail.com>
2022-02-27 20:42:44 +0100

    [fix] Adjust the mobile exp.

--- web/themes/custom/h22_gin/js/main.js
+++ web/themes/custom/h22_gin/js/main.js
@@ -3,3 +3,19 @@
  *
  * Extra main.js for the admin theme.
  */
+/* eslint-disable func-names, no-mutable-exports, comma-dangle, strict */
+
+'use strict';
+
+(($, Drupal, drupalSettings) => {
+  Drupal.behaviors.h22ginToolbarToggle = {
+    attach: function attach(context) {
+      // Toolbar toggle
+      $('.page-wrapper', context).on('click', function (e) {
+        if (window.matchMedia("(max-width: 767px)").matches && $('body').hasClass('toolbar-tray-open')) {
+          $('.toolbar-icon-menu').trigger('click');
+        }
+      });
+    }
+  };
+})(jQuery, Drupal, drupalSettings);
